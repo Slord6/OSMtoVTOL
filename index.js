df1bdfc245b907cfbc4cb6339e726d396d181f8e
Sam Lord <samlord6@gmail.com>
2022-04-02 13:33:47 +0100

    Rename API file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const API = require('./query')
+const API = require('./overpassAPI')
 
 const rawQuery = fs.readFileSync('./query.txt').toString();
 let query = API.injectBbox(rawQuery, 50.36835019940018,-4.320030212402344,50.46985198362652,-4.138927459716797)
